11ed36ddd5c0136b08e56d347e23340be44fc23f
KimJinWon <rlawlsdnjswk@naver.com>
2017-09-08 12:43:36 +0900

    Revert "구웨에엥 ㄱ 출력"
    This reverts commit 2ffc57bd169192129e75ca43d3fe7f6103ce0142.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-        System.out.println("구와아아아ㅏㄱ");
-
-        System.out.println("궤에에엑");
+	// write your code here
     }
 }
